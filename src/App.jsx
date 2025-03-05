5e85fe5d316847874718789847cda52bf6849389
Darkhan B <darkhanbekuzak13@gmail.com>
2025-03-05 15:01:01 +0500

    rnore

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import Header from "./components/Header/Header";
 import "./App.css";
 import Hero from "./components/Hero/Hero";
-import Slider from "./components/Hero/Slider";
 
 function App() {
   const [count, setCount] = useState(0);
